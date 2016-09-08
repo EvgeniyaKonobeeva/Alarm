e80fbf9de501931731c7e5d6c49f358d6c63e43e
Evgeniya Konobeeva <zknbeeva@mail.ru>
2016-09-09 01:42:17 +0300

    drawing circle with big and small pointers

--- app/src/main/java/com/example/ekonobeeva/alarm/PointerCircle.java
+++ app/src/main/java/com/example/ekonobeeva/alarm/PointerCircle.java
@@ -7,7 +7,10 @@ import android.graphics.DashPathEffect;
 import android.graphics.Paint;
 import android.graphics.Path;
 import android.graphics.PathDashPathEffect;
+import android.graphics.PathEffect;
+import android.graphics.SumPathEffect;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.View;
 
 /**
@@ -16,9 +19,15 @@ import android.view.View;
 public class PointerCircle extends View {
     private Paint mPaint;
     private Path mPath;
-    private Path shapePath;
-
-    private DashPathEffect mDashPathEffect;
+    private Path smallPointsPath;
+    private Path bigPointsPath;
+    private int actualWidth;
+    private int actualHeight;
+    private int radius;
+    private int spaceSmallPoints;
+    private int spaceBigPoints;
+    private int viewCenterX;
+    private int viewCenterY;
 
 
     public PointerCircle(Context context){
@@ -41,14 +50,18 @@ public class PointerCircle extends View {
         init();
     }
 
+
+
+    @Override
+    public void onWindowFocusChanged(boolean hasWindowFocus) {
+        super.onWindowFocusChanged(hasWindowFocus);
+        setMeasurement();
+    }
+
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
         canvas.drawARGB(80, 102, 204, 255);
-        //canvas.translate(150, 150);
-        canvas.drawPath(mPath, mPaint);
-        mPath.addCircle(250,250, 50, Path.Direction.CCW);
-        mPaint.setColor(Color.GREEN);
         canvas.drawPath(mPath, mPaint);
 
     }
@@ -58,17 +71,47 @@ public class PointerCircle extends View {
         mPaint.setStyle(Paint.Style.STROKE);
         mPaint.setAntiAlias(true);
         mPaint.setColor(Color.RED);
-        mPaint.setStrokeWidth(3);
+        mPaint.setStrokeWidth(5);
 
-        shapePath = new Path();
-        shapePath.addCircle(10,10, 5, Path.Direction.CCW);
+        smallPointsPath = new Path();
+        smallPointsPath.addCircle(0,0, 3, Path.Direction.CCW);
+
+        bigPointsPath = new Path();
+        bigPointsPath.addCircle(0,0, 9, Path.Direction.CCW);
+    }
+
+    public void setMeasurement(){
+        actualWidth = getMeasuredWidth();
+        actualHeight = getMeasuredHeight();
+
+        viewCenterX = actualWidth/2;
+        viewCenterY = actualHeight/2;
+
+        Log.d("CENTER X" , "" + viewCenterX);
+        Log.d("CENTER Y" , "" + viewCenterY);
+
+        radius = viewCenterX > viewCenterY ? viewCenterY : viewCenterX;
+        Log.d("РАДИУС" , "" + radius);
+
+        int circleLength = (int)Math.ceil(2*radius*Math.PI);
+
+        spaceSmallPoints = circleLength/60;
+        spaceBigPoints = spaceSmallPoints*5;
+
+        radius = (int )((spaceSmallPoints*60)/(2*Math.PI));
+        Log.d("РАДИУС" , "" + radius);
 
         mPath = new Path();
+        mPath.addCircle(viewCenterX,viewCenterY, radius, Path.Direction.CCW);
 
-        mPath.addCircle(550,550, 100, Path.Direction.CCW);
+        PathDashPathEffect smallPointsEffect = new PathDashPathEffect(this.smallPointsPath, spaceSmallPoints, 0, PathDashPathEffect.Style.ROTATE );
+        PathDashPathEffect bigPointsEffect = new PathDashPathEffect(this.bigPointsPath, spaceBigPoints, 2, PathDashPathEffect.Style.ROTATE );
+        PathEffect sumBigSmallEffect = new SumPathEffect(smallPointsEffect, bigPointsEffect);
 
-        mPaint.setPathEffect(new PathDashPathEffect(shapePath, 20, 0, PathDashPathEffect.Style.ROTATE));
+        mPaint.setPathEffect(sumBigSmallEffect);
+
     }
+
 
 //    @Override
 //    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
